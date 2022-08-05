469499300286f19e0e03dff0abdb2dcfd3e37953
ahmad <eng.ahmed.anwar@gmail.com>
2022-08-05 15:11:08 +1200

    deliver lecture

--- week1/fri-pm/fs-demo.test.js
+++ week1/fri-pm/fs-demo.test.js
@@ -2,8 +2,39 @@ const path = require('path');
 
 const fsFuncs = require('./fs-demo');
 
-test('getTheGoods reads contents of file passed to it', (done) => {});
+test('getTheGoods reads contents of file passed to it', (done) => {
+  // ARRANGE
+  const filePath = path.join(__dirname, 'testdata.txt');
 
-// test('getTheGoods throws an error when the file path is empty', (done) => {});
+  // ACT
+  fsFuncs.getTheGoods(filePath, (err, data) => {
+    // ASSERT
+    expect(data).toBe('HI Pohutukawa - 2022');
+    done();
+  });
+});
 
-// test('getTheGoods calls callback with generic error on error', (done) => {});
+test('getTheGoods throws an error when the file path is empty', (done) => {
+  // ARRANGE
+  const filePath = null;
+
+  //ACT
+  fsFuncs.getTheGoods(filePath, (err) => {
+    // ASSERT
+    expect(err.message).toBe('file path is empty');
+    done();
+  });
+});
+
+test('getTheGoods calls callback with generic error on error', (done) => {
+  // ARRANGE
+  const filePath = 'I do not exist';
+
+  // ACT
+  fsFuncs.getTheGoods(filePath, (err, contents) => {
+    // ASSERT
+    expect(err.message).toMatch('Oops');
+    expect(contents).toBeUndefined();
+    done();
+  });
+});
--- week1/fri-pm/index.js
+++ week1/fri-pm/index.js
@@ -1,15 +1,15 @@
-const path = require('path')
+const path = require('path');
 
-const fsFuncs = require('./fs-demo')
+const fsFuncs = require('./fs-demo');
 
-const dataPath = path.join(__dirname, 'testdata.txt')
+const dataPath = path.join(__dirname, 'fakefile.txt');
 
-function showContents (err, contents) {
+function showContents(err, contents) {
   if (err) {
-    console.error('ERROR!', err.message)
+    console.error('ERROR!', err.message);
   } else {
-    console.log('Contents:', contents)
+    console.log('Contents:', contents);
   }
 }
 
-fsFuncs.getTheGoods(dataPath, showContents)
+fsFuncs.getTheGoods(dataPath, showContents);

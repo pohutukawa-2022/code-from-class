2bfe3ee017bdeb66a0b626630c793c85bfe0c7f0
Alex Coaton <alex.coaton@devacademy.co.nz>
2022-08-08 10:59:44 +1200

    Blank slate for testing express routes with supertest.

---
+++ week2/mon-pm/README.md
@@ -0,0 +1,8 @@
+# Testing routes with SuperTest
+
+- Separate your server from making it listen
+- `server.js` contains the server config and the routes
+- `index.js` imports the server and makes it listen
+- `npm install jest supertest -D`
+- `touch server.test.js`
+- Test routes 🚀
---
+++ week2/mon-pm/frog.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Mr Sizzle',
+    type: 'leaf'
+  },
+  {
+    id: 2,
+    name: 'Susan',
+    type: 'poison dart'
+  },
+  {
+    id: 3,
+    name: 'Roxanne',
+    type: 'leaf'
+  },
+  {
+    id: 4,
+    name: 'Filip',
+    type: 'tailed'
+  }
+]

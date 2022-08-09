e17be6253fb483daa6e39bcd5629f44fbfeeac65
sebapian <sebapian95@gmail.com>
2022-08-09 14:10:37 +1200

    hey hey you you

--- week2/tue-am/server.js
+++ week2/tue-am/server.js
@@ -1,26 +1,39 @@
 // IMPORT THE BASICS
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
 // Import data
-const data = require('./data')
+const data = require('./data');
 
 // CREATE YOUR SERVER
-const server = express()
-module.exports = server
+const server = express();
+module.exports = server;
 
 // ADD PUBLIC FOLDER
-const publicFolder = path.join(__dirname, 'public')
-server.use(express.static(publicFolder))
-server.use(express.urlencoded({ extended: false }))
+const publicFolder = path.join(__dirname, 'public');
+server.use(express.static(publicFolder));
+server.use(express.urlencoded({ extended: false }));
+
+// CREATE A NEW VIEW ENGINE
+server.engine('hbs', engine({ extname: 'hbs' }));
+
+// CONNECT ENGINE TO SERVER
+server.set('view engine', 'hbs');
+server.set('views', './views');
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'old.html'))
-})
+  const viewData = {
+    blogs: data.blogs,
+    blogType: data.blogType,
+  };
+
+  res.render('home', viewData);
+});
 
 server.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'old-contact.html'))
-})
+  res.sendFile(path.join(__dirname, 'public', 'old-contact.html'));
+});
 
 server.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'old-about.html'))
-})
+  res.sendFile(path.join(__dirname, 'public', 'old-about.html'));
+});

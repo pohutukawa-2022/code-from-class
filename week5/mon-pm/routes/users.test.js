035143d2a2b07115dc2201a0799fbcac23c4b628
Alex Coaton <alex.coaton@devacademy.co.nz>
2022-08-29 15:21:45 +1200

    Testing Web APIs lesson complete.

--- week5/mon-pm/routes/users.test.js
+++ week5/mon-pm/routes/users.test.js
@@ -1,17 +1,68 @@
 const request = require('supertest')
+const server = require('../server')
+const db = require('../db')
+
+jest.mock('../db')
 
 test('GET /users returns all users', () => {
-  fail('Empty test.')
+  db.getUsers.mockImplementation(() => {
+    const users = [
+      { id: 99901, name: 'Karl', email: 'aardvark@example.org' },
+      { id: 99902, name: 'Oscar', email: 'baboon@example.org' },
+      { id: 99903, name: 'Ron', email: 'capybara@example.org' },
+    ]
+
+    return Promise.resolve(users)
+  })
+
+  return request(server)
+    .get('/users')
+    .expect(200) // OK
+    .then((res) => {
+      expect(res.body.users).toHaveLength(3)
+    })
 })
 
 test('GET /users/:id returns a single user', () => {
-  fail('Empty test.')
+  db.getUser.mockImplementation(() => {
+    const user = { id: 99901, name: 'Xavier', email: 'aardvark@example.org' }
+
+    return Promise.resolve(user)
+  })
+
+  return request(server)
+    .get('/users/3')
+    .expect(200)
+    .then((res) => {
+      expect(res.body.user.name).toBe('Xavier')
+    })
 })
 
 test('GET /users/:id should fail when passing a non-numeric id', () => {
-  fail('Empty test.')
+  return request(server)
+    .get('/users/camel')
+    .expect(500) // Internal error
+    .then((res) => {
+      expect(res.body.error).toBe('invalid id format')
+    })
 })
 
 test('POST /users should return 201 status code', () => {
-  fail('Empty test.')
+  db.addNewUser.mockImplementation((user) => {
+    console.log(user)
+    return Promise.resolve()
+  })
+
+  const userData = {
+    name: 'Bryden',
+    email: 'goingToMelbourne@here.com',
+  }
+
+  return request(server)
+    .post('/users')
+    .send(userData)
+    .expect(201) // Created
+    .then((res) => {
+      expect(res.status).toBe(201) // Created
+    })
 })

ac3524160ff26e28e8c713a94d887c7471595db9
clementine-smart <cmwsmart@gmail.com>
2022-08-30 13:12:17 +1200

    Update tue-pm starting code

--- week5/tue-pm/client/components/App.jsx
+++ week5/tue-pm/client/components/App.jsx
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import request from 'superagent'
 import { getFruits } from '../api'
 
 function App() {
-  const [fruit, setFruits] = useState([])
+  const [fruits, setFruits] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     getFruits()
       .then((res) => {
         setFruits(res.body)
+      })
+      .finally(() => {
+        setLoading(false)
       })
       .catch((err) => {
         console.error(`Oh no!: ${err.message}`)
       })
   }, [])
 
-  console.log(fruit)
+  console.log(fruits)
 
   return (
     <>
       <h1>Fruits</h1>
+      {loading ? (
+        <img src="busy.gif" />
+      ) : (
+        <ul>
+          {fruits.map((fruit) => {
+            return (
+              <li>{`${fruit.name} has ${fruit.nutritions.protein} grams of protein`}</li>
+            )
+          })}
+        </ul>
+      )}
     </>
   )
 }
